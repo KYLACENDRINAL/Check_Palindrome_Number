ec40e03cf64e28504adaa6ee37574828911087e9
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-22 01:31:57 +0800

    Done defining the function of palindrome

---
+++ Check_Palindrome_Number.py
@@ -0,0 +1,17 @@
+# Write a program to check if the given number is a palindrome number
+
+# pseudocode
+
+# Define the function of palindrome
+def palindrome(number):
+    original_number=number
+    reversed_number=int(str(number)[::1])
+
+    if original_number==reversed_number:
+        return f"Original number{original_number}\nYes. Given number is palindrome number"
+    else:
+        return f"Original number{original_number}\nNo. GIven number is not a palindrome number"
+    
+    
+# Give values
+# print the result
