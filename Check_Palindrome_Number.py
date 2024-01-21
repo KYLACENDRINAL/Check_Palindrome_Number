f0279d580a10dadb5b194bf8c1e74bad634dda6b
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-22 01:37:58 +0800

    Done printing result

--- Check_Palindrome_Number.py
+++ Check_Palindrome_Number.py
@@ -8,12 +8,16 @@ def palindrome(number):
     reversed_number=int(str(number)[::1])
 
     if original_number==reversed_number:
-        return f"Original number{original_number}\nYes. Given number is palindrome number"
+        return f"Original number: {original_number}\nYes. Given number is palindrome number"
     else:
-        return f"Original number{original_number}\nNo. GIven number is not a palindrome number"
+        return f"Original number: {original_number}\nNo. GIven number is not a palindrome number"
     
     
 # Give values
 value1=palindrome(121)
 value2=palindrome(125)
+
+# Print result
+print(value1)
+print(value2)
 
